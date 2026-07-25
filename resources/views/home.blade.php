98e2402aeb0f39e2bef221efa138b7e5375641ee
Tchris-Hub <tchrisdev@github.com>
2026-07-25 02:43:48 +0100

    fix: change hero button text from 'Book Now' to 'Apply Now' for consistency

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -150,7 +150,7 @@
     <h1>Welcome to <span>LincHostel.</span></h1>
     <h2>Where Comfort Meets Community, Affordable, Inclusive.</h2>
     <div class="d-flex">
-      <a href="{{ route('hostel.apply') }}" class="btn-get-started scrollto">Book Now</a>
+      <a href="{{ route('hostel.apply') }}" class="btn-get-started scrollto">Apply Now</a>
     </div>
   </div>
 </section>
